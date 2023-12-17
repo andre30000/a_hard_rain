a8bd62a57de1d38694eb0ce11258c59db46c20e9
andre30000 <andrecalaminus@gmail.com>
2023-12-16 22:46:36 -0600

    updated image routes

--- src/components/assets/imageAssets.jsx
+++ src/components/assets/imageAssets.jsx
@@ -3,13 +3,84 @@
 import image1 from './images/1.png';
 import image2 from './images/2.png';
 import image3 from './images/3.png';
+import image4 from './images/4.png';
+import image5 from './images/5.png';
+import image6 from './images/6.png';
+import image7 from './images/7.png';
+import image8 from './images/8.png';
+import image9 from './images/9.png';
+import image10 from './images/10.png';
+import image11 from './images/11.png';
+import image12 from './images/12.png';
+import image13 from './images/13.png';
+import image14 from './images/14.png';
+import image15 from './images/15.png';
+import image16 from './images/16.png';
+import image17 from './images/17.png';
+import image18 from './images/18.png';
+import image19 from './images/19.png';
+import image20 from './images/20.png';
+import image21 from './images/21.png';
+import image22 from './images/22.png';
+import image23 from './images/23.png';
+import image24 from './images/24.png';
+import image25 from './images/25.png';
+import image26 from './images/26.png';
+import image27 from './images/27.png';
+import image28 from './images/28.png';
+import image29 from './images/29.png';
+import image30 from './images/30.png';
+import image31 from './images/31.png';
+import image32 from './images/32.png';
+import image33 from './images/33.png';
+import image34 from './images/34.png';
+import image35 from './images/35.png';
+import image36 from './images/36.png';
+import image37 from './images/37.png';
+import image38 from './images/38.png';
+import image39 from './images/39.png';
 
 // Create an object mapping identifiers to images
 const imageAssets = {
     "1": image1,
     "2": image2,
     "3": image3,
-    // Add all other images following the same pattern
+    "4": image4,
+    "5": image5,
+    "6": image6,
+    "7": image7,
+    "8": image8,
+    "9": image9,
+    "10": image10,
+    "11": image11,
+    "12": image12,
+    "13": image13,
+    "14": image14,
+    "15": image15,
+    "16": image16,
+    "17": image17,
+    "18": image18,
+    "19": image19,
+    "20": image20,
+    "21": image21,
+    "22": image22,
+    "23": image23,
+    "24": image24,
+    "25": image25,
+    "26": image26,
+    "27": image27,
+    "28": image28,
+    "29": image29,
+    "30": image30,
+    "31": image31,
+    "32": image32,
+    "33": image33,
+    "34": image34,
+    "35": image35,
+    "36": image36,
+    "37": image37,
+    "38": image38,
+    "39": image39,
   };
   
   export default imageAssets;
